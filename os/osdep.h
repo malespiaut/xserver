543bf4ede765e77b137ea400644665cbddedafc7
Enrico Weigelt, metux IT consult <info@metux.net>
2025-11-07 16:42:29 +0100

    os: move OsCommRec/Ptr and related functions into own header
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- os/osdep.h
+++ os/osdep.h
@@ -61,27 +61,6 @@ SOFTWARE.
 # define __has_builtin(x) 0     /* Compatibility with older compilers */
 #endif
 
-typedef struct _connectionInput *ConnectionInputPtr;
-typedef struct _connectionOutput *ConnectionOutputPtr;
-
-typedef struct _osComm {
-    int fd;
-    ConnectionInputPtr input;
-    ConnectionOutputPtr output;
-    XID auth_id;                /* authorization id */
-    CARD32 conn_time;           /* timestamp if not established, else 0  */
-    struct _XtransConnInfo *trans_conn; /* transport connection object */
-    int flags;
-} OsCommRec, *OsCommPtr;
-
-int FlushClient(ClientPtr who, OsCommPtr oc);
-
-extern void FreeOsBuffers(OsCommPtr     /*oc */
-    );
-
-void
-CloseDownFileDescriptor(OsCommPtr oc);
-
 #include "dix.h"
 #include "ospoll.h"
 
